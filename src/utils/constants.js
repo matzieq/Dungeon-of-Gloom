39d40d4b5eb56150bd1868db1ed22c48cbcb54cf
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2020-04-21 21:28:40 +0200

    Add pre-generated map for testing purposes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const MAP_WIDTH = 80;
-export const MAP_HEIGHT = 40;
+export const MAP_HEIGHT = 50;
 export const SCREEN_HEIGHT = 50;
 export const FOG_UNEXPLORED = 0;
 export const FOG_VISIBLE = 1;
@@ -9,5 +9,5 @@ export const directions = [
   { x: -1, y: 0 },
   { x: 1, y: 0 },
   { x: 0, y: -1 },
-  { x: 0, y: 1 }
+  { x: 0, y: 1 },
 ];
